e7efe1c995f2286ca64f5b53a8b60916e3af4fac
Aldair Avalos UAM AZC <al2222005685@azc.uam.mx>
2023-06-20 21:05:08 -0600

    Game styles table of content

--- src/Components/Game/Game.jsx
+++ src/Components/Game/Game.jsx
@@ -2,23 +2,28 @@ import logo from "../../assets/images/logo.svg"
 import "./Game.css"
 
 function Game() {
-    return (
-        <section className="game">
-            <div className="options">
-                <button className="button">MENU</button>
-                <img className="logo" src={logo} alt="Logo" />
-                <button className="button">RESTART</button>
-            </div>
-            <div className="scores">
-                <button className="button">PLAYER1
-                    <div>12</div>
-                </button>
-                <button className="button">PLAYER2
-                    <div>23</div>
-                </button>
-            </div>
-        </section>
-    );
+  return (
+    <section className="game">
+      <div className="options">
+        <button className="button">MENU</button>
+        <img className="logo" src={logo} alt="Logo" />
+        <button className="button">RESTART</button>
+      </div>
+      <div className="scores">
+        <button className="button">PLAYER1
+          <div>12</div>
+        </button>
+        <button className="button">PLAYER2
+          <div>23</div>
+        </button>
+      </div>
+      <div className="board">
+        <div className="back"></div>
+        <div className="mid play-area"></div>
+        <div className="front"></div>
+      </div>
+    </section>
+  );
 }
 
 export default Game;
